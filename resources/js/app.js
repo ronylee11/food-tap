59395c95f9750f36ed5f800b775ec681aa9a7008
Rony Lee <rongyil33@gmail.com>
2024-01-14 13:48:46 +0800

    Connect css and js files to layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
-import './bootstrap';
+//import "./bootstrap";
+
+alert("Hello World!");
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <div class="container w-25 bg-primary p-5 border border-primary-subtle rounded">
+        {{--<h1>Hello World!</h1>--}}
+    </div>
+
+</x-layout>
