15065a8b0fb0728ffe28b020be5c426c9af283a3
Rony Lee <rongyil33@gmail.com>
2024-01-14 14:41:17 +0800

    Connect controller in route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    //return view('welcome');
-    return view('index');
-});
+Route::get('/', [IndexController::class, 'index']);
