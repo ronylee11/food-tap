ce39bc2dc1c3a626248aecb190af193ef2062f47
Rony Lee <rongyil33@gmail.com>
2024-01-14 14:45:35 +0800

    Add Index model

---
+++ app/Models/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Index extends Model
+{
+    use HasFactory;
+
+    protected $fillables = ['title', 'description'];
+
+    public static function all() {
+        return [
+            [
+            'title' => 'Hello World',
+            'description' => 'This is a description'
+            ],
+            [
+            'title' => 'Food Paradise',
+            'description' => 'cheap and affordable food!'
+            ]
+        ];
+    }
+}
