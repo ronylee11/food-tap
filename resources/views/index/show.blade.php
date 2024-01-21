ddb6329bcfec664fc5ad8b066aa91c60cf679c2d
Rony Lee <rongyil33@gmail.com>
2024-01-21 10:04:05 +0800

    Add show page

---
+++ resources/views/index/show.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+    <div class="container bg-primary p-5 border border-primary-subtle rounded">
+        <h1>ID: {{$index->id}}</h1>
+        <h2>Title: {{$index->title}}</h2>
+        <h3>Description: {{$index->description}}</h3>
+    </div>
+
+</x-layout>
