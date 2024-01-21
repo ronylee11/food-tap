ddd8624e570e8e743a88679da4d45d55c3106920
Rony Lee <rongyil33@gmail.com>
2024-01-21 09:56:51 +0800

    Add index folder in views

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="container bg-primary p-5 border border-primary-subtle rounded">
-        <h1>Hello World!</h1>
+        {{--<h1>Hello World!</h1>--}}
         {{--@dd ($indexs)--}}
         @foreach ($indexs as $index)
         <h1>{{ $index['title'] }}</h1>
