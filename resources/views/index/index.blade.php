f9cb1cb74281395fa4f1930e33efac289221ee76
Rony Lee <rongyil33@gmail.com>
2024-01-24 11:40:25 +0800

    Add button in index

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -3,9 +3,14 @@
         {{--<h1>Hello World!</h1>--}}
         {{--@dd ($indexs)--}}
         @foreach ($indexs as $index)
-        <h1>{{ $index['title'] }}</h1>
-        <h2>{{ $index['description'] }}</h1>
-            @endforeach
+        <div class="border m-2">
+            <form action="/{{$index['id']}}">
+                <h1>{{ $index['title'] }}</h1>
+                <h2>{{ $index['description'] }}</h1>
+                    <button type="submit" class="btn btn-primary">Read More</button>
+            </form>
+        </div>
+        @endforeach
     </div>
 
 </x-layout>
