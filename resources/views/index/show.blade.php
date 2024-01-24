5aa333e7dc5d5e3a61d2b17571ae4852b19e8221
Rony Lee <rongyil33@gmail.com>
2024-01-24 11:47:34 +0800

    Add back button in show page

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -1,10 +1,11 @@
 <x-layout>
 
     <div class="container bg-primary p-5 border border-primary-subtle rounded">
-        <h1>ID: {{$index->id}}</h1>
-        <h2>Title: {{$index->title}}</h2>
-        <h3>Description: {{$index->description}}</h3>
         <form action="/{{$index->id}}/edit" method="get">
+            <h1>ID: {{$index->id}}</h1>
+            <h2>Title: {{$index->title}}</h2>
+            <h3>Description: {{$index->description}}</h3>
+            <a class="btn btn-success" href="/">Back</a>
             <button type="submit" class="btn btn-secondary">Edit</button>
         </form>
     </div>
