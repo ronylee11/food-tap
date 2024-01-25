3be33cbe50be1e4a75db3327a7e4a1227cfd7b63
Rony Lee <rongyil33@gmail.com>
2024-01-25 11:46:22 +0800

    Add js that changes title color on click

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,10 @@
 //import "./bootstrap";
 
-alert("Hello World!");
+//alert("Hello World!");
+const title = document.querySelector("#title");
+
+const color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+title.addEventListener("click", () => {
+    title.style.color = color[Math.floor(Math.random() * color.length)];
+});
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,12 +6,15 @@
     <title>FoodTap</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset(" css/app.css")}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
 </head>
 
 <body {{$attributes->merge(['class'=>"bg-dark min-vh-100"])}}>
     {{$slot}}
-    <script src="{{asset(" js/app.js")}}"></script>
+    <script src="{{asset("js/app.js")}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
