d4346f9e031a0d12e4f5bb6643f0d8b80abc16f1
Rony Lee <rongyil33@gmail.com>
2024-01-28 17:42:09 +0800

    Change card text to align left

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div {{$attributes->merge(['class' => "border bg-secondary text-light m-2 d-flex justify-content-center
+<div {{$attributes->merge(['class' => "border bg-secondary text-light m-2 d-flex justify-content-start
     align-items-center p-3"])}}>
     {{$slot}}
 </div>
