ea614810a914b3f34704b71a99093032c0dbef8a
Rony Lee <rongyil33@gmail.com>
2024-01-28 17:46:29 +0800

    Change show form background color to white

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
 
-    <div class="container bg-primary p-5 border border-primary-subtle rounded">
-        @csrf
+    <div class="container bg-white p-5 border border-primary-subtle rounded">
         <h1>ID: {{$index->id}}</h1>
         <h2>Title: {{$index->title}}</h2>
         <h3>Description: {{$index->description}}</h3>
