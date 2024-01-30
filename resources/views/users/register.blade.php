a7b4958737d61735266a7bebdda17ddcaf66029b
Rony Lee <rongyil33@gmail.com>
2024-01-30 13:08:31 +0800

    Add register form

---
+++ resources/views/users/register.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-card class="flex-column justify-content-start">
+        <h1>Login</h1>
+        <label for="id">ID:</label>
+        <input type="text" id="id" name="id">
+    </x-card>
+</x-layout>
