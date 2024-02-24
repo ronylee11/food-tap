c1745fb8754e4adfeea2a77137e1e3a293516659
Rony Lee <rongyil33@gmail.com>
2024-02-24 21:48:25 +0800

    Move search bar to top

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,11 +1,10 @@
 <x-layout class="d-flex justify-content-center align-items-center flex-column">
+    @include('partials._search')
 
     <div class="container bg-white p-5 border border-primary-subtle rounded">
         <h1 class="fs-1" id="title">FoodTap</h1>
         <h2 class="fs-2">FoodTap is a food blog that provides you with the best recipes from around the world.</h2>
     </div>
-
-    @include('partials._search')
 
     <div class="container bg-white p-5 border border-primary-subtle rounded">
         {{--<h1>Hello World!</h1>--}}
